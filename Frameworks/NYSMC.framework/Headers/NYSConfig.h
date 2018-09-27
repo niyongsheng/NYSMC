9d70f6420ef96bdaacd9bab1abf88c5b926b598b
niyongsheng <niyongsheng@outlook.com>
2018-09-27 11:16:59 +0800

    ### ——mark——### *** 1. ** [GitHub](https://github.com/niyongsheng) ### ——end——###

---
+++ Frameworks/NYSMC.framework/Headers/NYSConfig.h
@@ -0,0 +1,29 @@
+//
+//  NYSConfig.h
+//  cake
+//
+//  Created by 倪刚 on 2018/9/24.
+//  Copyright © 2018年 NiYongsheng. All rights reserved.
+//
+
+#ifndef NYSConfig_h
+#define NYSConfig_h
+
+#define NYSCakeBundle(file) [@"sound.bundle" stringByAppendingPathComponent:file]
+#define NYSCakeResources(file) [@"images.bundle" stringByAppendingPathComponent:file]
+
+#define NYSC_ScreenWidth [UIScreen mainScreen].bounds.size.width
+#define NYSC_ScreenHeight [UIScreen mainScreen].bounds.size.height
+
+#define NYSC_Height_TabBar ((NYSC_Height_StatusBar > 20) ? 83 : 49)
+#define NYSC_Height_StatusBar CGRectGetHeight([[UIApplication sharedApplication] statusBarFrame])
+
+#define NYSC_Version_Key @"CFBundleShortVersionString"
+
+#ifdef DEBUG
+#define NLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
+#else
+#define NLog(...)
+#endif
+
+#endif /* NYSConfig_h */

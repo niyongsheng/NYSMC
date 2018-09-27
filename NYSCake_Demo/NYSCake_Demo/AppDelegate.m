1df604c01666bc72def1ff52162d59ce5437bfe5
niyongsheng <niyongsheng@outlook.com>
2018-09-27 19:29:45 +0800

    ### ——mark——### *** 1. demo updata** [GitHub](https://github.com/niyongsheng) ### ——end——###

--- NYSCake_Demo/NYSCake_Demo/AppDelegate.m
+++ NYSCake_Demo/NYSCake_Demo/AppDelegate.m
@@ -21,9 +21,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // 初始化NYSC
-    [NYSCake initWithEstimatedAuditDays:2 PostURL:@"http://chicken.qmook.com:8080/api/shelf/getReviewData2" ValidateParameters:nil BootMethod:NYSCBootMethod_Hot];
+    [NYSCake initWithEstimatedAuditDays:2 PostURL:@"http://chicken.qmook.com:8080/api/shelf/getReviewData2" ValidateParameters:nil BootMethod:NYSCBootMethod_Cold];
     // 选择分支
-    [NYSCake chooseViewControllerWithPriorityType:NYSCPriorityType_Server_Status errorBootFromType:BootFrom_Application matchSheelBlock:^{
+    [NYSCake chooseViewControllerWithPriorityType:NYSCPriorityType_Server_Status errorBootFromType:BootFrom_Sheel matchSheelBlock:^{
         self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[NYSCSheelViewController alloc] init]];
         [self.window makeKeyAndVisible];
     } ApplicationBlock:^{
